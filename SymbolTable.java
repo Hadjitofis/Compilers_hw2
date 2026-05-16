0f7beea2c17c9857f9db5fa5eb35c7a8777ee9e4
Hadjitofis <andhadjitofis@gmail.com>
2026-05-16 19:59:33 +0300

    moving to visitors

--- SymbolTable.java
+++ SymbolTable.java
@@ -7,7 +7,7 @@ public class SymbolTable{
     //for new classes declaration
     public void addClass(String name, String parent) throws Exception {
         if (classes.containsKey(name))
-            throw new Exception("Error: Class '"+name+"'declared twice.");
+            throw new Exception("Class '"+name+"'declared twice.");
         classes.put(name,new ClassInfo(name,parent));
     }
     // gets a class by its name
@@ -40,12 +40,34 @@ class MethodInfo{
     public List<String> paramTypes=new ArrayList<>();
     public List<String> paramNames=new ArrayList<>();
 
-    public Linked LinkedHashMap<String, String> locals= new LinkedHashMap<>();
+    public LinkedHashMap<String, String> local = new LinkedHashMap<>();
     public MethodInfo(String name, String returnType){
         this.name = name;
         this.returnType=returnType;
     }
+    //add a parameter or throw if dublicated
+    public void addParam(String pname, String ptype)throws Exception{
+        if(paramNames.contains(pname))
+            throw new Exception("Parameter '"+pname+"'already exists in method");
+        paramNames.add(pname);
+        paramTypes.add(ptype);
 
+    }
+    //add a parameter or throw if dublicated variable name
+    public void addlocal(String vname,String vtype) throws Exception{
+        if(local.containsKey(vname))
+            throw new Exception("Local variable '"+vname+"'already exists in method");
+        local.put(vname,vtype);
+    }
+
+    public String searchvar(String vname){
+        if (local.containsKey(vname))
+            return local.get(vname);
+        
+        int i=paramNames.indexOf(vname);
+        if(i>=0)return paramTypes.get(i);
+        return null;
+    }
 
 }
 //To store all the information about classes
@@ -63,12 +85,20 @@ class ClassInfo{
     }
     //Creates new field or throw exception if it already exists
     public void newField(String nname,String ntype) throws Exception{
-        if(fields.containKey(nname))
-        throw new Exception("Error:Field '"+nname+ "'already exists in class'"+name);
+        if(fields.containsKey(nname))
+        throw new Exception("Field '"+nname+ "'already exists in class'"+name);
         fields.put(nname,ntype);
     }
-    //
-    public void addMethod(String nname,Methodinfo info){
+    //add a new method
+    public void addMethod(String nname,MethodInfo info){
         methods.put(nname+"/"+info.paramTypes.size(),info);
+    }
+    //find fieldtype  by name
+    public String getFieldType(String fname){
+        return fields.get(fname);
+    }
+    //get methods info by name and arguments
+    public MethodInfo getMethod(String mname,int argCount){
+        return methods.get(mname+"/"+argCount);
     }
 }
